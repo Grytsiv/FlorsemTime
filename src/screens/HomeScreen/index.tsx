21a84d56de7a94e81754fffd7ee8b8e7d842c9c3
Andrii Hrytsiv <andriygrytsiv1@gmail.com>
2025-12-24 12:34:16 +0200

    Home Screen minor changes

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {FlatList, View} from 'react-native';
-import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {useFocusEffect} from '@react-navigation/native';
 import {ICompanyResponse} from '../../models/ICreateLicenseModel.ts';
 import {TRootState} from '../../boot/configureStore.ts';
 import {useAppDispatch, useAppSelector} from '../../boot/hooks';
@@ -12,48 +12,49 @@ type ScreenProps = {
   navigation: any;
 };
 
-const HomeScreen: React.FC<ScreenProps> = () => {
-    const dispatch = useAppDispatch();
-    const navigation = useNavigation();
+const HomeScreen: React.FC<ScreenProps> = ({navigation}) => {
+  const dispatch = useAppDispatch();
 
-    const {isBusy} = useAppSelector((state: TRootState) => state.appServiceReducer);
-    const {paymentList} = useAppSelector((state: TRootState) => state.homeReducer);
+  const {isBusy} = useAppSelector(
+    (state: TRootState) => state.appServiceReducer,
+  );
+  const {paymentList} = useAppSelector(
+    (state: TRootState) => state.homeReducer,
+  );
 
-    const handleCardPress = (item: ICompanyResponse) => {
-        dispatch(ActionCreators.clearLastPayment());
-        // @ts-ignore
-        navigation.navigate('CompanyDetailsScreen', {item});
-    };
+  const handleCardPress = (item: ICompanyResponse) => {
+    dispatch(ActionCreators.clearLastPayment());
+    navigation.navigate('CompanyDetailsScreen', {item});
+  };
 
-    const onRefresh = React.useCallback(() => {
-          dispatch(ActionCreators.handlePaymentList());
-    }, [dispatch]);
+  const onRefresh = React.useCallback(() => {
+    dispatch(ActionCreators.handlePaymentList());
+  }, [dispatch]);
 
-    useFocusEffect(
-        React.useCallback(() => {
-            onRefresh();
-        }, [onRefresh])
-    );
+  useFocusEffect(
+    React.useCallback(() => {
+      onRefresh();
+    }, [onRefresh]),
+  );
 
-    return (
-        <View style={styles.container}>
-          <FlatList
-              contentOffset={{y: isBusy ? -60 : 0, x: 0}} // <-- if loading, set offset, this fix list shift after loading
-              data={paymentList}
-              keyExtractor={(item, index) => item.data.type.toString() + index.toString()}
-              refreshing={isBusy}
-              onRefresh={onRefresh}
-              contentContainerStyle={styles.contentContainer}
-              scrollIndicatorInsets={styles.scrollIndicatorInsets}
-              renderItem={({item}) => (
-                  <CompanyCard
-                      item={item}
-                      onPress={() => handleCardPress(item)}
-                  />
-              )}
-          />
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <FlatList
+        contentOffset={{y: isBusy ? -60 : 0, x: 0}} // <-- if loading, set offset, this fix list shift after loading
+        data={paymentList}
+        keyExtractor={(item, index) =>
+          item.data.type.toString() + index.toString()
+        }
+        refreshing={isBusy}
+        onRefresh={onRefresh}
+        contentContainerStyle={styles.contentContainer}
+        scrollIndicatorInsets={styles.scrollIndicatorInsets}
+        renderItem={({item}) => (
+          <CompanyCard item={item} onPress={() => handleCardPress(item)} />
+        )}
+      />
+    </View>
+  );
 };
 
 HomeScreen.displayName = 'HomeScreen';
